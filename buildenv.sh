a8d7cc8f2363d2a37230b415dda7df543634a3d2
great <cloudi@naver.com>
2023-03-07 14:36:53 +0000

    Add buildscript

---
+++ buildenv.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function find_top_dir()
+{
+    local TOPDIR=poky
+# move into script file path
+    cd $(dirname ${BASH_SOURCE[0]})
+    
+    if [ -d $TOPDIR ]; then
+        echo $(pwd) 
+    else
+        while [ ! -d $TOPDIR ] && [ $(pwd) != "/" ];
+        do
+             cd ..
+        done
+
+        if [ -d $TOPDIR ]; then
+            echo $(pwd)
+        else
+            echo "/dev/null"
+        fi
+    fi
+}
+
+ROOT=$(find_top_dir)
+export TEMPLATECONF=${ROOT}/poky/meta-great/template/
+
+function build_target() {
+    source poky/oe-init-build-env build2
+}
+
+build_target
